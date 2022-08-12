5c4ce3ec9ef9784a63af4787ca0fc2049694dca6
Julian <mayorga.julian@gmail.com>
2022-08-12 08:53:15 -0300

    Replace graphiql with gql ide on `/api/graphql`

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,6 +2,7 @@ import { createServer } from "@graphql-yoga/node";
 import { readFileSync } from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { join } from "path";
+import { renderGraphiQL } from "@graphqlapps/ide";
 
 export const config = {
   api: {
@@ -13,6 +14,7 @@ export default createServer<{
   req: NextApiRequest;
   res: NextApiResponse;
 }>({
+  renderGraphiQL,
   schema: {
     typeDefs: readFileSync(join(process.cwd(), "schema.graphql"), {
       encoding: "utf-8",
