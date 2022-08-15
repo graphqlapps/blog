6e646b99685124078559ee1d26fb7f907e34b79c
Julian <mayorga.julian@gmail.com>
2022-08-15 18:36:11 -0300

    Host images locally on Tus instead of Transloadit

--- src/uppy.ts
+++ src/uppy.ts
@@ -1,6 +1,7 @@
 import Uppy from "@uppy/core";
 import Transloadit from "@uppy/transloadit";
 import { useMemo } from "react";
+import Tus from "@uppy/tus";
 
 export const useUppy = ({
   expires,
@@ -16,23 +17,30 @@ export const useUppy = ({
       autoProceed: true,
       restrictions: { maxNumberOfFiles: 1 },
     });
-    if (signature && expires && key) {
-      uppy.use(Transloadit, {
-        waitForEncoding: true,
-        signature,
-        params: {
-          auth: {
-            key,
-            expires,
-          },
-          steps: {
-            // https://transloadit.com/docs/topics/assembly-instructions/#order-of-execution
-            ":original": {
-              robot: "/upload/handle",
+    if (process.env.NODE_ENV === "development") {
+      uppy.use(Tus, {
+        endpoint: "http://localhost:1080/files/",
+        retryDelays: [0, 1000, 3000, 5000],
+      });
+    } else {
+      if (signature && expires && key) {
+        uppy.use(Transloadit, {
+          waitForEncoding: true,
+          signature,
+          params: {
+            auth: {
+              key,
+              expires,
+            },
+            steps: {
+              // https://transloadit.com/docs/topics/assembly-instructions/#order-of-execution
+              ":original": {
+                robot: "/upload/handle",
+              },
             },
           },
-        },
-      });
+        });
+      }
     }
     return uppy;
   }, [signature, expires, key]);
